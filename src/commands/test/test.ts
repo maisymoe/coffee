a1d7cf2777499dead18b4444abf5739943d5cf95
Beef <beefers.tv@gmail.com>
2022-01-02 16:34:22 +0000

    [Commands > Test] Remove unused client reference and old commented code

--- src/commands/test/test.ts
+++ src/commands/test/test.ts
@@ -1,51 +1,11 @@
 import { Command } from "../../lib/def";
-import { CommandInteraction, Message, MessageEmbed } from "discord.js";
-import { client } from "../../index";
+import { CommandInteraction } from "discord.js";
 
 export default new Command({
     name: "test",
     description: "test",
     category: "test",
-    // options: [
-    //     {
-    //         name: "autocomplete",
-    //         description: "i have no idea what i am doing",
-    //         type: "STRING",
-    //         choices: [
-    //             {
-    //                 name: "Choice 1",
-    //                 value: "choice1",
-    //             },
-    //             {
-    //                 name: "Choice 2",
-    //                 value: "choice2",
-    //             },
-    //         ]
-    //     },
-    // ],
     async execute(interaction: CommandInteraction): Promise<any> {
-        // client.on("messageCreate", (message) => {
-        //     if (!message.content.startsWith("VCTEST:")) return;
-            
-        //     message.reply(`You said ${message.content}`);
-        // })
-
-        // const channel = await client.channels.fetch("878677833935892510");
-        // if (!channel?.isText()) return console.log("Channel is not a text channel");
-
-        // try {
-        //     channel?.send("test");
-        // } catch(error) {
-        //     console.log(error);
-        // }
-
-        // function sleep(ms: number) {
-        //     return new Promise(resolve => setTimeout(resolve, ms));
-        // }
-
-        // await interaction.editReply({ content: "I will be leaving in 5 seconder" })
-        // sleep(5000);
-        // await interaction.guild?.leave();
-        return await interaction.editReply({ content: "heck." });
+        return await interaction.editReply({ content: "Test command. heck." });
     },
 });
